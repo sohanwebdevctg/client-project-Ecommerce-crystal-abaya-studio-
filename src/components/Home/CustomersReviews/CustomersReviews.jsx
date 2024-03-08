41a80b6f72e1354e31540b65e1c1bd1a501ca73a
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-03-08 16:37:25 +0600

    adding json data

--- src/components/Home/CustomersReviews/CustomersReviews.jsx
+++ src/components/Home/CustomersReviews/CustomersReviews.jsx
@@ -1,6 +1,24 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import { Pagination, Navigation } from 'swiper/modules';
+import { useEffect } from 'react';
 
 const CustomersReviews = () => {
+
+
+  // data fetch
+  useEffect(() => {
+    fetch('customersReviews.json')
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+  })
+
+
   return (
     <div className="my-10">
       {/* customersReviews container section start */}
@@ -11,7 +29,27 @@ const CustomersReviews = () => {
         {/* title section end */}
         {/* user details section start */}
         <div>
-          
+          <Swiper
+          slidesPerView={1}
+          spaceBetween={30}
+          loop={true}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Pagination, Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+          </Swiper>
         </div>
         {/* user details section end */}
         {/* content section end */}
